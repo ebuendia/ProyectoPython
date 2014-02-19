5f208c04c8a5d299775d04255c889e3d0673ee79
joseluisbueno <jbueno@espol.edu.ec>
2014-02-19 03:28:25 -0500

    Agregado el modulo capability.py con sus respectivos metodos

---
+++ src/capability.py
@@ -0,0 +1,30 @@
+class Capability:
+	def __init__(self, name, value):
+		setName(name)
+		setValue(value)
+		self.__group = ""
+
+	# Metodos Getters
+	def getName(self):
+		return str(self.__name)
+
+	def getValue(self):
+		return str(self.__value)
+
+	def getGroup(self):
+		return str(self.__group)
+
+	# Metodos Setters
+	def setName(self, name):
+		self.__name = name
+
+	def setValue(self, value):
+		self.__value = value
+
+	def setGroup(self, group):
+		self.__group = group
+
+	# Metodos Sobrecargados
+	def __str__(self):
+		salida = "Name: {0}\nValue: {1}\n"
+		return salida.format(self.getName(), self.getValue())
